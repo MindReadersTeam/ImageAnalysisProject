72fa16762405d497ff76e8a3da536f5d025d7586
Michał Szczepańczyk <25662022+Shaid3r@users.noreply.github.com>
2018-12-24 16:49:02 +0100

    Add image processing script

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # ImageAnalysisProject - web app
 
 Simple web application that recognizes several hand gestures displayed by the user.
+
+
+## Installation
+
+```bash
+sudo apt-get install python3-venv
+python3 -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Usage
+```python3
+from img_processing import process_dir
+process_dir('../imgs/in', '../imgs/out', True)
+```
---
+++ backend/img_processing.py
@@ -0,0 +1,56 @@
+# /usr/bin/env python3
+
+from pathlib import Path
+import skimage
+from skimage import io
+from skimage.filters import sobel
+from skimage.filters.rank import mean_bilateral
+from skimage.morphology import disk
+
+_info = False
+
+
+def info(msg, end='\n'):
+    global _info
+    if _info:
+        print(msg, end=end)
+
+
+def set_info(flag):
+    global _info
+    _info = flag
+
+
+def load_image(fn, gray=True):
+    return skimage.img_as_float64(io.imread(fn, as_gray=gray))
+
+
+def process_image(img, disk_radius=20):
+    return sobel(mean_bilateral(img, disk(disk_radius)))
+
+
+def process_dir(input_dir, output_dir, verbose=False):
+    """
+    Processes every image in 'input_dir' and results saves in 'output_dir'.
+    Processing is executed only on images that not exists in output dir or
+    with newer modification time.
+    """
+    set_info(verbose)
+    ip = Path(input_dir)
+    op = Path(output_dir)
+
+    for path in ip.iterdir():
+        info("Processing: {}".format(path.name), end='')
+        output_path = op / path.name
+        if output_path.exists() and output_path.stat().st_mtime > path.stat().st_mtime:
+            info(" => skip")
+            continue
+
+        img = load_image(path.as_posix())
+        img = process_image(img)
+        io.imsave(output_path.as_posix(), img)
+        info(" => done")
+
+
+if __name__ == "__main__":
+    pass
