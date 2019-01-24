6c0c38ac5b33455267c0513c5e0c796c1cd66b53
root <root@mindreader.localdomain>
2019-01-24 20:43:15 +0000

    update models

--- backend/models.py
+++ backend/models.py
@@ -44,3 +44,49 @@ class ModelFactory:
 #        model.add(Dense(1))
 #        model.add(Activation('sigmoid')) 
         return model
+
+    def get_model3(self):
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3), input_shape=self.init_shape))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(32, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(64, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(self.num_classes, activation='softmax'))
+#        model.add(Dense(1))
+#        model.add(Activation('sigmoid'))
+        return model
+
+    def get_model4(self):
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=self.init_shape))
+        model.add(Conv2D(32, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+     
+        model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+     
+        model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+     
+        model.add(Flatten())
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(self.num_classes, activation='softmax'))
+        return model
--- backend/neural_network.py
+++ backend/neural_network.py
@@ -22,10 +22,10 @@ types = [
     'call_me'
 ]
 
-batch_size = 2
+batch_size = 32
 num_classes = 10
 epochs = 12
-img_width, img_height = 640, 640
+img_width, img_height = 320, 320
 
 if K.image_data_format() == 'channels_first':
     input_shape = (1, img_width, img_height)
@@ -33,10 +33,11 @@ else:
     input_shape = (img_width, img_height, 1)
 
 modelFactory = models.ModelFactory(input_shape, num_classes)
-model = modelFactory.get_model2()
+model = modelFactory.get_model3()
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
+              #optimizer='adam',rmsprop
               metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(rescale=1. / 255)
@@ -56,18 +57,37 @@ validation_generator = test_datagen.flow_from_directory(
     color_mode='grayscale'
 )
 
-model.fit_generator(
+model.summary()
+
+history = model.fit_generator(
     train_generator,
-    steps_per_epoch=4000 // batch_size,
+    steps_per_epoch=train_generator.n // batch_size,
     epochs=epochs,
     validation_data=validation_generator,
-    validation_steps=2000 // batch_size,
+    validation_steps=validation_generator.n // batch_size,
     verbose=2
     #use_multiprocessing=True
 )
 
 model.save_weights('first_try.h5')
 
+## Loss Curves
+#plt.figure(figsize=[8,6])
+#plt.plot(history.history['loss'],'r',linewidth=3.0)
+#plt.plot(history.history['val_loss'],'b',linewidth=3.0)
+#plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
+#plt.xlabel('Epochs ',fontsize=16)
+#plt.ylabel('Loss',fontsize=16)
+#plt.title('Loss Curves',fontsize=16)
+#
+## Accuracy Curves
+#plt.figure(figsize=[8,6])
+#plt.plot(history.history['acc'],'r',linewidth=3.0)
+#plt.plot(history.history['val_acc'],'b',linewidth=3.0)
+#plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+#plt.xlabel('Epochs ',fontsize=16)
+#plt.ylabel('Accuracy',fontsize=16)
+#plt.title('Accuracy Curves',fontsize=16)
 
 # score = model.evaluate(x_test, y_test, verbose=0)
 # print('Test loss:', score[0])
