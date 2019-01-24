9d054cb0aff4a4bfdbcd66c64825f573d2bc7e86
Name=Szczepanczyk Michal <miszczep@krling44.emea.nsn-net.net>
2019-01-24 23:20:10 +0100

    Add first trained model

--- backend/neural_network.py
+++ backend/neural_network.py
@@ -5,6 +5,7 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 import models
+from matplotlib import pyplot as plt
 
 train_data = "./imgs/splitted/train"
 validation_data = "./imgs/splitted/validation"
@@ -25,7 +26,7 @@ types = [
 batch_size = 32
 num_classes = 10
 epochs = 12
-img_width, img_height = 320, 320
+img_width, img_height = 128, 128
 
 if K.image_data_format() == 'channels_first':
     input_shape = (1, img_width, img_height)
@@ -33,7 +34,7 @@ else:
     input_shape = (img_width, img_height, 1)
 
 modelFactory = models.ModelFactory(input_shape, num_classes)
-model = modelFactory.get_model3()
+model = modelFactory.get_modelo1()
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
@@ -65,29 +66,33 @@ history = model.fit_generator(
     epochs=epochs,
     validation_data=validation_generator,
     validation_steps=validation_generator.n // batch_size,
-    verbose=2
-    #use_multiprocessing=True
+    verbose=2,
+    use_multiprocessing=True
 )
 
 model.save_weights('first_try.h5')
 
-## Loss Curves
-#plt.figure(figsize=[8,6])
-#plt.plot(history.history['loss'],'r',linewidth=3.0)
-#plt.plot(history.history['val_loss'],'b',linewidth=3.0)
-#plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
-#plt.xlabel('Epochs ',fontsize=16)
-#plt.ylabel('Loss',fontsize=16)
-#plt.title('Loss Curves',fontsize=16)
-#
-## Accuracy Curves
-#plt.figure(figsize=[8,6])
-#plt.plot(history.history['acc'],'r',linewidth=3.0)
-#plt.plot(history.history['val_acc'],'b',linewidth=3.0)
-#plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
-#plt.xlabel('Epochs ',fontsize=16)
-#plt.ylabel('Accuracy',fontsize=16)
-#plt.title('Accuracy Curves',fontsize=16)
+# Loss Curves
+plt.figure(figsize=[8,6])
+plt.plot(history.history['loss'],'r',linewidth=3.0)
+plt.plot(history.history['val_loss'],'b',linewidth=3.0)
+plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Loss',fontsize=16)
+plt.title('Loss Curves',fontsize=16)
+
+plt.savefig('loss.png')
+
+# Accuracy Curves
+plt.figure(figsize=[8,6])
+plt.plot(history.history['acc'],'r',linewidth=3.0)
+plt.plot(history.history['val_acc'],'b',linewidth=3.0)
+plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Accuracy',fontsize=16)
+plt.title('Accuracy Curves',fontsize=16)
+
+plt.savefig('accuracy.png')
 
 # score = model.evaluate(x_test, y_test, verbose=0)
 # print('Test loss:', score[0])
