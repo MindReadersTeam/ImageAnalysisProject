c0caabdb7fefe6bb39113d77b7df6389bcbb8f7e
root <root@mindreader.localdomain>
2019-02-01 23:33:17 +0000

    Add predicting backend

--- backend/run.py
+++ backend/run.py
@@ -8,13 +8,16 @@ def hello():
 @app.after_request
 def after_request(response):
     header = response.headers
-    header["Access-Control-Allow-Origin"] = "https://mindreaders.ml"
+    #header["Access-Control-Allow-Origin"] = "https://mindreaders.ml"
+    header["Access-Control-Allow-Origin"] = "*"
     header["Access-Control-Allow-Headers"] = "content-type"
     header["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     return response
 
 from img_uploading import upload_api
+from img_predicting import predict_api
 app.register_blueprint(upload_api)
+app.register_blueprint(predict_api)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
